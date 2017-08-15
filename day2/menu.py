a338e143430f5267dc8be2e8b5ad51ec5635c936
rivest829 <956605485@qq.com>
2017-08-15 16:11:10 +0800

    三级菜单完成！

--- day2/menu.py
+++ day2/menu.py
@@ -43,14 +43,32 @@ menu={'Peking':{
 
 exit_flag=True
 while exit_flag:
-    print(menu)
+    for i in menu:
+        print(i)
     command=input('What u want:')
     if command in menu:
-        print(menu[command])
-        command2=input('what u want:')
         while exit_flag:
-            print(menu[command][command2])
-            command3=input('what u want:')
-
+            for i in menu[command]:
+                print(i)
+            command2 = input('what u want:')
+            if command2 in menu[command]:
+                while exit_flag:
+                    for i in menu[command][command2]:
+                        print(i)
+                    command3=input('what u want:')
+                    for i in menu[command][command2]:
+                        print(i)
+                    if command3=='b':
+                        break
+                    elif command3=='q':
+                        exit_flag=False
+                    else:
+                        print('Wrong input')
+            elif command2 == 'b':
+                break
+            elif command2 == 'q':
+                exit_flag = False
+            else:
+                print('Wrong input')
     elif command=='q':
         exit_flag=False
