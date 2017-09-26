02769045a6c08bd640bf6d6a0ec010f829072388
rivest829 <956605485@qq.com>
2017-09-26 16:44:10 +0800

    learn python origan

--- day3/1fileIO.py
+++ day3/1fileIO.py
@@ -1,2 +1,4 @@
 # -*- coding:utf-8 -*-
 # Author Hsinhan Chiang
+file1=open("antistand",encoding="utf-8").read()
+print(file1)
---
+++ day3/3progress bar.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+# Author Hsinhan Chiang
+import sys
+import time
+for i in range(500):
+    if i%50 == 0:
+        sys.stdout.write('\r\n')
+    sys.stdout.write('#')
+    sys.stdout.flush()
+    time.sleep(0.1)
